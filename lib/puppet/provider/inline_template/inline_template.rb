53e74534935868e50b1a2b6cd3e06389709f8262
monk-ee <lyndon.swan@flightcentre.com.au>
2013-08-20 10:30:55 +1000

    fixing logic for mode

--- lib/puppet/provider/inline_template/inline_template.rb
+++ lib/puppet/provider/inline_template/inline_template.rb
@@ -17,7 +17,7 @@ Puppet::Type.type(:inline_template).provide(:inline_template) do
   			file.puts template_content
 		end
 		#now set mode if it exists
-		if defined? #{@resource[:mode]}
+		if defined? @resource[:mode]
 		    File.chmod("#{@resource[:mode]}","#{@resource[:name]}","out")
 		end
 
