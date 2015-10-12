954ca1252c66e7447cb2b6d2b385543ab1e78004
msolonkovich26 <msolonkovich@boardbooks.com>
2015-10-12 11:12:55 -0400

    Adding better logic for changes

--- lib/puppet/provider/inline_template/inline_template.rb
+++ lib/puppet/provider/inline_template/inline_template.rb
@@ -6,6 +6,17 @@ Puppet::Type.type(:inline_template).provide(:inline_template) do
   def exists?
     # better check that the local template exists
     return false unless File.exists?("#{@resource[:source]}")
+    if (File.exists?("#{@resource[:name]}"))
+      template = ERB.new(File.read("#{@resource[:source]}"))
+      template_content = template.result(binding).strip
+      currentfile = File.open("#{@resource[:name]}", "r:bom|utf-8")
+      currentfile_content = currentfile.read
+      current_content = currentfile_content.strip
+      currentfile.close
+      if(current_content != template_content)
+        File.delete("#{@resource[:name]}")
+      end
+    end
     if (@resource[:mode] != "" && File.exists?("#{@resource[:name]}"))
       File.chmod(Integer("#{@resource[:mode]}"),"#{@resource[:name]}")
     end
@@ -13,10 +24,10 @@ Puppet::Type.type(:inline_template).provide(:inline_template) do
 
   def create
     #ok grab the local params and pass them into templater
-    template = ERB.new(File.read("#{@resource[:source]}"))
-    template_content = template.result(binding)
+    template_create = ERB.new(File.read("#{@resource[:source]}"))
+    template_create_content = template_create.result(binding)
     File.open("#{@resource[:name]}", "w") do |file|
-      file.puts template_content
+      file.puts template_create_content
     end
     #now set mode if it exists
     if @resource[:mode] != ""
